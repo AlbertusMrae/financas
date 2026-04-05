4305b6c32a569b07b471da5c43335b1c2b183b57
AlbertusMrae <albertusestvir@gmail.com>
2026-04-05 14:25:43 -0400

    fix(lista-compras): adiciona suporte a rls

--- lib/features/lista_compras/data/models/item_compra_model.dart
+++ lib/features/lista_compras/data/models/item_compra_model.dart
@@ -3,6 +3,7 @@ import '../../domain/entities/item_compra.dart';
 class ItemCompraModel extends ItemCompra {
   const ItemCompraModel({
     required super.id,
+    required super.casalId,
     required super.descricao,
     required super.marcado,
   });
@@ -10,12 +11,18 @@ class ItemCompraModel extends ItemCompra {
   factory ItemCompraModel.fromJson(Map<String, dynamic> json) {
     return ItemCompraModel(
       id: json['id'] as String,
+      casalId: json['casal_id'] as String,
       descricao: json['descricao'] as String,
       marcado: json['marcado'] as bool,
     );
   }
 
   Map<String, dynamic> toJson() {
-    return {'id': id, 'descricao': descricao, 'marcado': marcado};
+    return {
+      'id': id,
+      'casal_id': casalId,
+      'descricao': descricao,
+      'marcado': marcado,
+    };
   }
 }
--- lib/features/lista_compras/domain/entities/item_compra.dart
+++ lib/features/lista_compras/domain/entities/item_compra.dart
@@ -1,10 +1,12 @@
 class ItemCompra {
   final String id;
+  final String casalId;
   final String descricao;
   final bool marcado;
 
   const ItemCompra({
     required this.id,
+    required this.casalId,
     required this.descricao,
     required this.marcado,
   });
